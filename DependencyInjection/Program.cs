8e89595d63fee5a04c6002cf0de5ed549a68e42f
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2025-09-13 13:16:08 +0300

    update

--- DependencyInjection/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using DependencyInjection;
-
-IOgretmen ogretmen = new Ogretmen("Ayşe", "Demir");
-ClassRoom classRoom = new ClassRoom(ogretmen);
-
-Console.WriteLine(classRoom.GetTeacherInfo());
-
-
